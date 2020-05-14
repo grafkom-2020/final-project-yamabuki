5d6b4dfa6181e5e1a5f4ef0982c119fff598f27c
Marsellaeve <marsella.eve@gmail.com>
2020-05-14 12:31:39 +0700

    ruler

--- yamabookey/main.js
+++ yamabookey/main.js
@@ -1,3 +1,6 @@
+var ketinggian=0;
+var height=4.8;
+var height2=-0.2;
 function main(){
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,0.1,1000);
@@ -21,7 +24,7 @@ function main(){
     scene.add(sprite); 
 
     camera.position.z = 3;
-
+    
     function getRandomInt(max) {
         return Math.floor(Math.random() * Math.floor(max));
     }
@@ -34,10 +37,29 @@ function main(){
         sprite1.scale.set(5,5,5);
         scene.add(sprite1);
     };
-
+    
+    var spriteMap2 = new THREE.TextureLoader().load("Asset.png");
+    var spriteMat2 = new THREE.SpriteMaterial({map:spriteMap2});
+    var sprite2 = new THREE.Sprite(spriteMat2);
+    var ruler = function(x,y){
+        sprite2.position.set(x,y,0);
+        sprite2.scale.set(1,5,1);
+        scene.add(sprite2);
+    };
+    var spriteMap3 = new THREE.TextureLoader().load("Asset.png");
+    var spriteMat3 = new THREE.SpriteMaterial({map:spriteMap2});
+    var sprite3 = new THREE.Sprite(spriteMat3);
+    var ruler2 = function(x,y){
+        sprite3.position.set(x,y,0);
+        sprite3.scale.set(1,5,1);
+        scene.add(sprite3);
+    };
     var clock = new THREE.Clock();
     var delta = 0;
     var timeToSpawn=5;
+    var timetoheight=1;
+    var timetoheight2=0.3;
+
     //fungsi update
     var update = function(){
         delta = clock.getDelta();
@@ -49,20 +71,45 @@ function main(){
             spawnEnemy(getRandomInt(4)-2, x);
             timeToSpawn=10;
         }
+        timetoheight-=delta; //waktu kecepatan tinggi
+        if(timetoheight<0){
+            ketinggian+=10;
+            timetoheight=0.5;
+        }
+        timetoheight2-=delta;
+        if(timetoheight2<0){
+            timetoheight2=0.05;
+            ruler(-3.4,4.8-height%10);
+            height+=0.1;
+            ruler2(-3.4,4.8-height2%10);
+            height2+=0.1;
+        }
     };
+    
+    var text2 = document.createElement('div');
+    var tinggi = function(){
+        text2.style.position = 'absolute';
+        text2.style.fontSize = 35;
+        text2.style.fontWeight='bold';
+        text2.innerHTML = ketinggian + ' m';
+        text2.style.top = 45 + 'px';
+        text2.style.left = 80 + 'px';
+        document.body.appendChild(text2);
+    }
 
     //fungsi render
     var render = function(){
         renderer.render(scene,camera);
+        tinggi();
     };
 
     var GameLoop = function(){
         requestAnimationFrame(GameLoop);
 
+
         update();
         render();
     };
-
     GameLoop();
 }
 
